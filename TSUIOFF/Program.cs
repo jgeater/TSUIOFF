e2c909f85bbacf52ab416e454d05023f9e48396e
James Geater <jgeater@outlook.com>
2018-08-20 09:52:05 -0400

    Initial commit

---
+++ TSUIOFF/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSUIOFF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Type tsProgress = Type.GetTypeFromProgID("Microsoft.SMS.TSProgressUI");
+                dynamic comObject = Activator.CreateInstance(tsProgress);
+
+                comObject.CloseProgressDialog();
+
+                if (System.Runtime.InteropServices.Marshal.IsComObject(comObject) == true)
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(comObject);
+            }
+            catch
+            {
+                Console.WriteLine("Task sequence environment not found!");
+            }
+        }
+    }
+}
